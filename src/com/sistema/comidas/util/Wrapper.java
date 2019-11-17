bb1854e0ba8fe6b7689ffba0fb5c606b8853c8a1
Destroyer <joaquin121416@gmail.com>
2019-11-17 18:35:01 -0500

    Signed-off-by: Destroyer <joaquin121416@gmail.com>

---
+++ src/com/sistema/comidas/util/Wrapper.java
@@ -0,0 +1,30 @@
+package com.sistema.comidas.util;
+
+import org.displaytag.decorator.TableDecorator;
+import org.omg.PortableServer.POAPackage.ObjectAlreadyActive;
+
+import com.sistema.comidas.bean.CategoriaProducto;
+
+public class Wrapper extends TableDecorator{
+
+	
+	public String getSeleccionaCategoria() {
+		CategoriaProducto  obj = (CategoriaProducto) getCurrentRowObject();
+		
+		String selecciona ="<a class=\"btn btn-primary btn-sm\" href=\"/LP_2_Sistema_Venta_3.0/CategoriaServlet?opc=act1&codigo="+ obj.getCat_pro_id()+"\" value=\"\">\r\n" + 
+				"											<i class=\"fa fa-dot-circle-o\"></i> Listar\r\n" + 
+				"										</a>";
+		return selecciona;
+		
+	}
+	
+	public String getEliminaCategoria() {
+		CategoriaProducto  obj = (CategoriaProducto) getCurrentRowObject();
+		
+		String selecciona ="<button class=\"btn btn-primary btn-sm\" href=\"/LP_2_Sistema_Venta_3.0/CategoriaServlet?opc=eli&codigo="+ obj.getCat_pro_id()+"\" value=\"\">\r\n" + 
+				"											<i class=\"fa fa-dot-circle-o\"></i> Eliminar\r\n" + 
+				"										</button>";
+		return selecciona;
+		
+	}
+}
