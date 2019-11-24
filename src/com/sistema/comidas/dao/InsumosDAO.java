473f092d48e96ee75c7b5bce0b995b48ab6019b8
Alexander B <57377200+EL111@users.noreply.github.com>
2019-11-24 12:31:57 -0500

    Add files via upload

---
+++ src/com/sistema/comidas/dao/InsumosDAO.java
@@ -0,0 +1,17 @@
+package com.sistema.comidas.dao;
+
+import java.util.ArrayList;
+
+import com.sistema.comidas.bean.InsumosBean;
+public interface InsumosDAO {
+
+	
+	public int agregarInsumo(InsumosBean ins);
+	
+	public ArrayList<InsumosDAO>listarInsumos();
+
+	public int modificarInsumos(InsumosBean ins);
+	
+	public int eliminarInsumo(InsumosBean ins);
+	public InsumosBean listarByID(int id);
+}
