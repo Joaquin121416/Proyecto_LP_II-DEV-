38f37ea8201994c115a5cfb7c61e333998f18f21
Alexander B <57377200+EL111@users.noreply.github.com>
2019-11-24 12:28:57 -0500

    Add files via upload

---
+++ src/com/sistema/comidas/servlets/InsumoServlet.java
@@ -0,0 +1,140 @@
+package com.sistema.comidas.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sistema.comidas.bean.CategoriaProductoBean;
+import com.sistema.comidas.bean.InsumosBean;
+import com.sistema.comidas.dao.CategoriaDAO;
+import com.sistema.comidas.dao.InsumosDAO;
+import com.sistema.comidas.dao.factory.Factory;
+@WebServlet("/InsumoServlet")
+public class InsumoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Entro al servlet Producto");
+		String opc = request.getParameter("opc");
+		switch(opc) {
+		case "lis": listarInsumo(request,response);break;
+		case "fil": filtrarInsumo(request,response);break;
+		case "reg": registrarInsumo(request,response);break;
+		case "act": actualizarInsumo(request,response);break;
+		case "eli": eliminarInsumo(request,response);break;
+		}
+	}
+	
+	private void eliminarInsumo(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void actualizarInsumo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+			String id = String.valueOf(request.getParameter("codigo"));
+			int cod = Integer.parseInt(id);
+			Factory bd = Factory.getTipo(Factory.TIPO_MYSQL);
+			InsumosDAO dao = (InsumosDAO) bd.getInsumoDAO();
+			InsumosBean ins;
+			ins = dao.listarByID(cod);
+			String url = "Insumos/ActualizarInsumoMenu.jsp";
+			request.setAttribute("INS_ID",ins.getINS_ID());
+			request.setAttribute("INS_NOM", ins.getINS_NOM());
+			request.setAttribute("INS_DES", ins.getINS_DES());
+			request.setAttribute("INS_PRE", ins.getINS_PRE());
+			request.getRequestDispatcher(url).forward(request, response);
+
+		}
+	private void registrarInsumo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Entro a registrar");
+		String mensaje, url;
+		String INSNOM;
+		String INS_DES;
+		double INS_PRE;
+
+		
+		INSNOM = request.getParameter("txtNombre");
+		INS_DES = request.getParameter("txtDescripcion");
+		INS_PRE = Double.parseDouble(request.getParameter("txtPrecio"));
+		
+
+		
+		InsumosBean p = new InsumosBean(0, INS_DES, INS_DES, INS_PRE);
+		p.setINS_NOM(INSNOM);
+		p.setINS_DES(INS_DES);
+		p.setINS_PRE(INS_PRE);
+		
+		System.out.println(p);
+		
+		Factory fabrica = Factory.getTipo(Factory.TIPO_MYSQL);
+		InsumosDAO dao =fabrica.getInsumoDAO();
+		int ok = dao.agregarInsumo(p);
+		
+		if(ok==0) {
+			mensaje = "Error al registrar Insumo";
+			url = "Insumos/AgregarInsumo.jsp";
+		}else {
+			mensaje = "Insumo " + p.getINS_ID() + " registrado";
+			url = "Insumos/AgregarInsumoMenu.jsp";
+		}
+		
+		request.setAttribute("msg", mensaje);
+		request.getRequestDispatcher(url).forward(request, response);
+		
+	}
+
+	private void filtrarInsumo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String mensaje = "", url;
+		int tipo = Integer.parseInt(request.getParameter("cboTipoInsumo"));
+		
+		Factory fabrica = Factory.getTipo(Factory.TIPO_MYSQL);
+		InsumosDAO dao = fabrica.getInsumoDAO();
+		ArrayList<InsumosDAO> lista = dao.listarInsumos();
+		
+		if (lista.isEmpty()) {
+			mensaje = "no se encontraron Insumos  ";
+			url = "Insumos/ListaInsumoMenu.jsp";
+		} else {
+			mensaje = "";
+			url = "Insumos/ListaInsumoMenu.jsp";
+		}
+		
+		// salidas
+		request.setAttribute("aviso", mensaje);
+		request.setAttribute("listadoins", lista);
+		request.getRequestDispatcher(url).forward(request, response);
+	}
+	
+	private void listarInsumo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// variables
+				String mensaje = null;
+				String url = null;
+				Factory bd = Factory.getTipo(Factory.TIPO_MYSQL);
+				InsumosDAO dao =bd.getInsumoDAO();
+				ArrayList<InsumosDAO> lis;
+				lis=dao.listarInsumos();
+				
+				if (lis.isEmpty()) {
+					mensaje = "no se encontraron Insumos que listar";
+					url = "Insumos/ListaInsumoMenu.jsp";
+				} else {
+					mensaje = "";
+					url = "Insumos/ListaInsumoMenu.jsp";
+				}
+				
+				// salidas
+				request.setAttribute("lista", lis);
+				request.setAttribute("mensaje", mensaje);
+				request.getRequestDispatcher(url).forward(request, response);
+			
+			}
+}
